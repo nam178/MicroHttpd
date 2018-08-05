6d1a1698b5b7054f29c45d466247ea1d42dbb23c
Nam Duong <nam.duong.887@gmail.com>
2018-08-05 18:09:08 +1000

    Polishing the demo web page

--- Demo/DemoProgram.cs
+++ Demo/DemoProgram.cs
@@ -2,7 +2,6 @@
 using log4net.Config;
 using MicroHttpd.Core;
 using MicroHttpd.Core.StringMatch;
-using System.IO;
 using System.Reflection;
 
 namespace Demo
@@ -11,23 +10,40 @@ namespace Demo
     {
 		static void Main(string[] args)
 		{
-			// Enable logging
+			// Enable logging if you like
 			BasicConfigurator.Configure(
 				LogManager.CreateRepository(
 					Assembly.GetEntryAssembly(),
 					typeof(log4net.Repository.Hierarchy.Hierarchy)));
 
+			// Create the HTTP server, 
+			// it doesn't do anything until you configure and start it.
 			var httpService = HttpServiceFacade.Create();
-			httpService.AddSSL(RelativeToAssembly("ssl/microhttp.localhost.pfx"), ",g9e/~ArH=aH.k8C");
+
+#if SSL
+			// Configure the server to use SSL,
+			// specifying path to the PFX file and its password.
+			httpService.AddSSL(
+				PathUtils.RelativeToAssembly("ssl/cert.pfx"), 
+				",g9e/~ArH=aH.k8C");
+#endif
+
+			// Configure virtual host - the domain name,
+			// the web root and ports.
 			httpService.AddVirtualHost(new VirtualHostConfig
 			{
 				// We'll set document root to the www directory of this project.
-				DocumentRoot = RelativeToAssembly("../../../www"),
+				DocumentRoot = PathUtils.RelativeToAssembly("www"),
 
-				// Accept all host names
+				// Accept all host names,
+				// Other than MatchAll, you can also use
+				// MicroHttpd.Core.StringMatch.Regex, or,
+				// MicroHttpd.Core.StringMatch.ExactCaseSensitive
 				HostName = new MatchAll(),
 
-				// Accept incoming connections on port 8443
+				// Accept incoming connections on port 8443,
+				// If you want to use port 443 and/or 80, add them here.
+				// (You'll need to start the application as root)
 				ListenOnPorts = new int[] { 8443 }
 			});
 
@@ -36,15 +52,6 @@ namespace Demo
 
 			// Wait, this prevents the console program from exiting.
 			httpService.Wait();
-		}
-
-		static string RelativeToAssembly(string path)
-		{
-			return Path.GetFullPath(
-				Path.Combine(
-					Path.GetDirectoryName(
-						Assembly.GetEntryAssembly().Location), 
-						path));
 		}
 	}
 }
---
+++ MicroHttpd.Core/PathUtils.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace MicroHttpd.Core
+{
+	public static class PathUtils
+    {
+		public static string RelativeToAssembly(string path)
+		{
+			return Path.GetFullPath(
+				Path.Combine(
+					Path.GetDirectoryName(
+						Assembly.GetEntryAssembly().Location),
+						path));
+		}
+	}
+}
